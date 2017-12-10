033ef8f255ba77e988ba5344af3c484dfc81bacb
rsj217 <rsj217@gmail.com>
2017-12-10 14:38:34 +0800

    ADD: binary node

---
+++ tree/node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+class BinNode(object):
+
+    def __init__(self, parent=None, lchild=None, rchild=None, data=None):
+        self.parent = parent
+        self.lchild = lchild
+        self.rchild = rchild
+        self.data = data
+
+    def insert_as_lc(self, e):
+        node = BinNode(parent=self, data=e)
+        self.lchild = node
+        return node
+
+    def insert_as_rc(self, e):
+        node = BinNode(parent=self, data=e)
+        self.rchild = node
+        return node
+
+    @property
+    def size(self):
+        s = 1
+        if self.lchild:
+            s += self.lchild.size()
+        if self.rchild:
+            s += self.rchild.size()
+        return s
+
+    def succ(self):
+        pass
