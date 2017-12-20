f9db6e42af21d930ec743152932e3c4ec4f71eba
rsj217 <rsj217@gmail.com>
2017-12-20 17:39:04 +0800

    ADD: add balance binary search tree base class

---
+++ tree/balance_binary_search_tree.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from tree.node import BSNode
+from tree.binary_search_tree import BinarySearchTree
+
+
+class BalanceBinarySearchTree(BinarySearchTree):
+    pass
+
+
+class AVLTree(BalanceBinarySearchTree):
+    pass
+
+
+def gen_avl_tree():
+    """
+    <BinarySearchTree>(
+                                  36
+                  27                              58
+          16                              53              69
+                                      40              64
+                                        46
+    )
+    """
+    avl = AVLTree()
+    avl.insert_as_root(36)
+
+    avl.insert(27)
+    avl.insert(16)
+    avl.insert(58)
+    avl.insert(53)
+    avl.insert(40)
+    avl.insert(69)
+    avl.insert(64)
+
+    return avl
+
+
+if __name__ == '__main__':
+    avl = gen_avl_tree()
+    print(avl)
