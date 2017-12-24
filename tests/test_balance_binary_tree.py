602fd94fd5952eccba0bc2a4556eca90a79fd406
rsj217 <rsj217@gmail.com>
2017-12-24 20:16:03 +0800

    ADD: avl remove

--- tests/test_balance_binary_tree.py
+++ tests/test_balance_binary_tree.py
@@ -27,6 +27,10 @@ class TestAVLTree(unittest.TestCase):
         self.avl.insert(30)
         self.avl.insert(20)
 
+        g = self.avl.trave_level()
+        for i in g:
+            self.assertTrue(self.avl.avl_balanced(i))
+
     def test_remove_node(self):
         """
         <AVLTree>(
@@ -45,14 +49,9 @@ class TestAVLTree(unittest.TestCase):
         self.avl.insert(52)
 
         self.avl.remove(69)
-
-        print(self.avl)
-
         g = self.avl.trave_level()
         for i in g:
             self.assertTrue(self.avl.avl_balanced(i))
-
-
 
 
 if __name__ == '__main__':
