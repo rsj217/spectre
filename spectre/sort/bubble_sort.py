f656ca3b59e2e5c2927ec0c864b9411d93fd490f
rsj217 <rsj217@gmail.com>
2018-02-08 17:33:25 +0800

    common bubble sort

--- spectre/sort/bubble_sort.py
+++ spectre/sort/bubble_sort.py
@@ -4,12 +4,23 @@
 __author__ = 'master'
 
 
-def sort(l):
+def common_sort(l):
     for i in range(len(l) - 1):
         for j in range(len(l) - i - 1):
             if l[j] > l[j + 1]:
                 l[j], l[j + 1] = l[j + 1], l[j]
     return l
+
+
+def opm_sort(l):
+    is_sorted = False
+    for i in range(len(l) - 1):
+        if not is_sorted:
+            is_sorted = True
+            for j in range(len(l) - i - 1):
+                if l[j] > l[j + 1]:
+                    l[j], l[j + 1] = l[j + 1], l[j]
+                    is_sorted = False
 
 
 if __name__ == '__main__':
