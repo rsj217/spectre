1364193c82f53a5e54ca958092120692a1fd11fe
rsj217 <rsj217@gmail.com>
2018-02-08 17:23:17 +0800

    common bubble sort

--- spectre/sort/bubble_sort.py
+++ spectre/sort/bubble_sort.py
@@ -2,3 +2,15 @@
 # -*- coding:utf-8 -*-
 
 __author__ = 'master'
+
+
+def sort(l):
+    for i in range(len(l) - 1):
+        for j in range(len(l) - i - 1):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+    return l
+
+
+if __name__ == '__main__':
+    pass
