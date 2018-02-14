c2fc113c647f4f379db2a9d42b36ac47652305ba
rsj217 <rsj217@gmail.com>
2018-02-14 20:33:49 +0800

    avl tree

--- tests/test_balance_binary_tree.py
+++ tests/test_balance_binary_tree.py
@@ -2,7 +2,46 @@
 # -*- coding:utf-8 -*-
 
 import unittest
+from spectre.tree.node import Entry
 from spectre.tree.balance_binary_search_tree import AVLTree
+
+
+def gen_tree():
+    n58 = Entry(key=58, value=58)
+    avl = AVLTree()
+    avl.insert_as_root(n58)
+
+    n53 = Entry(key=53, value=53)
+    n40 = Entry(key=40, value=40)
+    n69 = Entry(key=69, value=69)
+    n54 = Entry(key=54, value=54)
+
+    avl.insert(n53)
+    avl.insert(n40)
+    avl.insert(n69)
+    avl.insert(n54)
+    return avl
+
+
+def gen_avl_tree():
+    n50 = Entry(key=50, value=50)
+    n30 = Entry(key=30, value=30)
+    n58 = Entry(key=58, value=58)
+    n20 = Entry(key=20, value=20)
+    n54 = Entry(key=54, value=54)
+    n69 = Entry(key=69, value=69)
+    n52 = Entry(key=52, value=52)
+
+    avl = AVLTree()
+    avl.insert_as_root(n50)
+
+    avl.insert(n30)
+    avl.insert(n58)
+    avl.insert(n20)
+    avl.insert(n54)
+    avl.insert(n69)
+    avl.insert(n52)
+    return avl
 
 
 class TestAVLTree(unittest.TestCase):
@@ -18,18 +57,13 @@ class TestAVLTree(unittest.TestCase):
                 54  69
         )
         """
-        root = self.avl.insert_as_root(58)
-
-        self.avl.insert(53)
-        self.avl.insert(40)
-        self.avl.insert(69)
-        self.avl.insert(54)
-        print(self.avl)
-        g = self.avl.trav_levelorder(self.avl.root)
+        avl = gen_tree()
+        print(avl)
+        g = avl.trav_levelorder(avl.root)
         for i in g:
-            self.assertTrue(self.avl.avl_balanced(i))
+            self.assertTrue(avl.avl_balanced(i))
 
-    def test_remove_node(self):
+    def test_remove_zig_zig(self):
         """
         <AVLTree>(
                       50
@@ -44,19 +78,61 @@ class TestAVLTree(unittest.TestCase):
         20      52  58
         )
         """
-        root = self.avl.insert_as_root(50)
-
-        self.avl.insert(30)
-        self.avl.insert(58)
-        self.avl.insert(20)
-        self.avl.insert(54)
-        self.avl.insert(69)
-        self.avl.insert(52)
-        self.avl.remove(69)
-        print(self.avl)
-        g = self.avl.trav_levelorder(self.avl.root)
+        avl = gen_avl_tree()
+        avl.remove(69)
+        print(avl)
+        g = avl.trav_levelorder(avl.root)
         for i in g:
-            self.assertTrue(self.avl.avl_balanced(i))
+            self.assertTrue(avl.avl_balanced(i))
+
+    def test_remove_zag_zig(self):
+        """
+        <AVLTree>(
+                      50
+              30              58
+          20              54      69
+                        52
+        )
+
+        <AVLTree>(
+              50
+          30      54
+        20      52  58
+        )
+        """
+        avl = gen_avl_tree()
+        avl.remove(30)
+        print(avl)
+        g = avl.trav_levelorder(avl.root)
+        for i in g:
+            self.assertTrue(avl.avl_balanced(i))
+
+    def test_remove_zig_zag(self):
+
+        avl = gen_avl_tree()
+        e25 = Entry(key=25, value=25)
+        avl.insert(e25)
+        avl.remove(50)
+        print(avl)
+        g = avl.trav_levelorder(avl.root)
+        for i in g:
+            self.assertTrue(avl.avl_balanced(i))
+
+
+    def test_remove_zag_zag(self):
+
+        avl = gen_avl_tree()
+        e21 = Entry(key=21, value=21)
+        e22 = Entry(key=35, value=22)
+        avl.insert(e21)
+        avl.insert(e22)
+        print(avl)
+        avl.remove(20)
+        print(avl)
+        g = avl.trav_levelorder(avl.root)
+        for i in g:
+            self.assertTrue(avl.avl_balanced(i))
+
 
 
 if __name__ == '__main__':
